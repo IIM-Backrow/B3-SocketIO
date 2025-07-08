2182dfd687f7c69353860e59db4d91fcc2779299
Thomas Candille <114737865+ThomasCandille@users.noreply.github.com>
2025-07-08 10:17:35 +0200

    feat: add queue management buttons and update socket event handling

--- frontend/scripts/socket.js
+++ frontend/scripts/socket.js
@@ -1,8 +1,33 @@
+const joinQueueButton = document.getElementById("join-queue");
+const leaveQueueButton = document.getElementById("leave-queue");
+const informativeText = document.getElementById("informative-text");
+
 socket = io("http://localhost:3000");
 socket.on("connect", () => {
   console.log("Connected to the server");
+  socket.emit("ping");
 });
 
 socket.on("disconnect", () => {
   console.log("Disconnected from the server");
+});
+
+joinQueueButton.addEventListener("click", () => {
+informativeText.textContent = "Waiting for an opponent...";
+    leaveQueueButton.disabled = false;
+    joinQueueButton.disabled = true;
+  socket.emit("join_queue");
+});
+
+leaveQueueButton.addEventListener("click", () => {
+  informativeText.textContent = "Click on Join Queue to participate";
+    leaveQueueButton.disabled = true;
+    joinQueueButton.disabled = false;
+  socket.emit("leave_queue");
+});
+
+socket.on("match_found", (gameId) => {
+  informativeText.textContent = `Match found! Game ID: ${gameId}`;
+  joinQueueButton.disabled = true;
+  leaveQueueButton.disabled = true;
 });
